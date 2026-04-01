a30002d1138a2ed684fd2b991a56813e6fa3d743
김정현 <hyeon020206@naver.com>
2026-04-01 19:48:27 +0900

    feat: add image viewer for room display

---
+++ src/ImageViewer.java
@@ -0,0 +1,29 @@
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import java.awt.Image;
+
+public class ImageViewer {
+    private JFrame frame;
+    private JLabel label;
+
+    public ImageViewer() {
+        frame = new JFrame("게임 화면");
+        label = new JLabel();
+
+        frame.add(label);
+        frame.setSize(520, 340);
+        frame.setLocation(50, 50);
+        frame.setAlwaysOnTop(true);
+        frame.setVisible(true);
+    }
+
+    public void showImage(String path) {
+        ImageIcon icon = new ImageIcon(path);
+        Image img = icon.getImage();
+        Image changeImg = img.getScaledInstance(500, 300, Image.SCALE_SMOOTH);
+        ImageIcon changeIcon = new ImageIcon(changeImg);
+
+        label.setIcon(changeIcon);
+    }
+}
